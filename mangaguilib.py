da598b9ebcc7feda8880c331004297aaf2cdff23
Sebastian Sammons <Fridge@Sebastians-MacBook-Pro.local>
2018-10-06 23:47:11 -0700

    Cleaned up both the main window and the copy logs window layout. Added simple and detailed log functionality to the gui. Did the bounds checking for last volume chapter field. Still need to work on error messages and hiding last volume chapter fields.

--- mangaguilib.py
+++ mangaguilib.py
@@ -4,6 +4,13 @@ import mangaformatlib
 from tkinter import *
 from tkinter import messagebox
 
+
+#
+#FOR ALL GUIS
+#Look into protocols for WM_DELETE_WINDOWS
+#
+'''
+ORIINAL FORMAL SELECT GUI
 def formatselectgui():
     masterwindow = Tk()
     masterwindow.title("Manga Formatter")
@@ -37,64 +44,148 @@ def formatselectgui():
     entrynumber.grid(row=3,column=6,sticky=E)
     
     #Create Button
-    b = Button(masterwindow, text="Format", command= lambda: confirmformat(formatvar,titlevar,entrynumber,masterwindow))
-    b.grid(row=3,column=7)
+    b = Button(masterwindow, text="Format", command= lambda: startformat(formatvar,titlevar,entrynumber,masterwindow))
+    b.grid(row=3,column=7, sticky=E)
 
+    #masterwindow.resizable(False, False)
+
+    #Draw window
+    masterwindow.mainloop()
+'''
+
+def formatselectgui():
+    masterwindow = Tk()
+    masterwindow.title("Manga Formatter")
+    masterwindow.geometry('350x100')
+
+    #Format Label
+    formatlabel = Label(masterwindow, text="Select Format")
+    formatlabel.grid(row=0,column=0,columnspan=100,sticky=E)
+    #Format Option List
+    formatlist = ["Chapter Format","Volume Format"]
+    formatvar=StringVar()
+    formatvar.set(formatlist[0])
+    formatoption = OptionMenu(masterwindow,formatvar, *formatlist)
+    formatoption.config(width=12)
+    formatoption.grid(row=0,column=100,columnspan=100,sticky=E)
+
+    #Manga Label
+    mangalabel = Label(masterwindow, text="Select Manga")
+    mangalabel.grid(row=1,column=0,columnspan=100,sticky=E)
+    #Manga Title Option List
+    titlelist = ["Attack On Titan","Boruto","Dr. Stone","My Hero Academia","One Piece","Platinum End","The Promised Neverland"] 
+    titlevar=StringVar()
+    titlevar.set(titlelist[0])
+    mangalist = OptionMenu(masterwindow,titlevar,*titlelist)
+    mangalist.config(width=18)
+    mangalist.grid(row=1,column=100,columnspan=100)
+
+    #Last Chapter of New Volume Label
+    lconvlabel = Label(masterwindow, text="Volume End At")
+    lconvlabel.grid(row=2,column=99,sticky=W)
+    #Last Chapter of New Volume Text Box
+    entrynumber = Entry(masterwindow)
+    entrynumber.config(width=4)
+    entrynumber.grid(row=2,column=100,sticky=W)
+    
+    #Create Button
+    b = Button(masterwindow, text="Format", command= lambda: startformat(formatvar,titlevar,entrynumber,masterwindow,rbv))
+    b.grid(row=2,column=199, sticky=E)
+
+    #Radiobuttons
+    rbv = IntVar()
+    rbd = Radiobutton(masterwindow, text = "Detailed",variable = rbv,value = 0)
+    rbs = Radiobutton(masterwindow, text = "Simple",variable = rbv, value = 1)
+    rbd.grid(row=3,column=99,sticky=E)
+    rbs.grid(row=3,column=100,sticky=W)
+    rbv.set(1)
+    #masterwindow.resizable(False, False)
 
     #Draw window
     masterwindow.mainloop()
 
 
+
 #Confirmation Button
-def confirmformat(formatvar,titlevar,entrynumber,root):
+def startformat(formatvar,titlevar,entrynumber,root,rbv):
+    if(rbv.get()==1) :
+        logtype = "Simple"
+    else :
+        logtype = "Detailed"
     formattype = formatvar.get()
     title = titlevar.get()
     number = entrynumber.get()
-    if(formatvar.get()=="Chapter Format"):
-        #logs = mangaformatlib.manual_format_chapter(title,number)
-        mangaformatlib.sql_format_chapter(title)
-    elif(formatvar.get()=="Volume Format"):
-        #Volume Format Function
-        mangaformatlib.sql_format_volume(title,number)
+    if(formattype=="Chapter Format"):
+        mangaformatlib.sql_format_chapter(title,logtype)
+    elif(formattype=="Volume Format"):
+        #check for empty field
+        if(number=="") :
+            error_message("NO CHAPTER NUMBER GIVEN")
+            return
+        #Make sure entry is an actual Number
+        try :
+            number = int(number)
+        except :
+            error_message("CHAPTER NUMBER NOT GIVEN")
+            return
+        #Make sure that lastchapter is within the bounds of the chapters available
+        try :
+            otherchaplist = [f for f in os.listdir(mangaformatlib.ROOTPATH + str(title) + mangaformatlib.CHAPTERFORMATPATH) if not f.startswith('.')]
+        except :
+            error_message("ERROR!! [" + mangaformatlib.QUEUESRC + "] NOT FOUND")
+            exit()
+        otherchaplist = sorted(otherchaplist)
+        if(int(number) > int(otherchaplist[len(otherchaplist)-1])) :
+            error_message("ENTRY GIVEN TOO BIG FOR FORMAT")
+            return
+        if(int(number) < int(otherchaplist[0])) :
+            error_message("ENTRY GIVEN TOO SMALL FOR FORMAT")
+            return    
+        mangaformatlib.sql_format_volume(title,number,logtype)
     else:
-        print("FATAL ERROR")
-    print("destroy format")
+        error_message("FATAL ERROR: NO FORMAT TYPE")
+    print("destroy first format window")
     root.destroy()
-    print("quit format")
+    print("quit first format window")
     root.quit()
-    #try:
-    #    display_logs(logs)  
-    #except:
-    #    print("no logs")
 
 def error_message(message):
     top = Tk()
     #Avoids root window from being drawn
     top.geometry("0x0")
-    messagebox.showwarning(message)
+    messagebox.showwarning("ERROR",message)
+    top.destroy()
 
-def display_logs(loglist):
-    listwindow = Tk()
-    listwindow.title("Format Logs")
-    listwindow.geometry("1800x400")
-    #Create Text
-    text = Text(listwindow,height = 20,width=250)
+def display_logs(loglist,logtype):
+    logwindow = Tk()
+    logwindow.title("Format Logs")
+    if(logtype=="Simple") :
+        logwindow.geometry("406x400")
+        text = Text(logwindow,height = 24,width=57,bg="grey")
+    else :
+        logwindow.geometry("1880x400")
+        #Create Text
+        text = Text(logwindow,height = 24,width=267,bg="grey")
     for i in loglist:
         text.insert(END, i + '\n')
-    text.pack()
+    #text.pack(side=LEFT, fill=BOTH, expand=YES)
     text.config(state="disabled")
-    text.grid(row=0,column=0)
+    text.grid(row=0,column=0,columnspan=200)
 
 
     #Create OK Button
     #make confirm format
-    bcommit = Button(listwindow, text="FORMAT", command= lambda: commitformat(listwindow))
-    bcommit.grid(row=1,column=1,sticky=E)
+    bcommit = Button(logwindow, text="FORMAT", command= lambda: commitformat(logwindow))
+    bcommit.grid(row=1,column=101,sticky=E)
 
-    babort = Button(listwindow, text ="ABORT", command= lambda: abortformat(listwindow))
-    babort.grid(row=1,column=0,sticky=W)
+    babort = Button(logwindow, text ="ABORT", command= lambda: abortformat(logwindow))
+    babort.grid(row=1,column=99,sticky=E)
+    
+    logwindow.protocol("WM_DELETE_WINDOW", lambda: abortformat(logwindow))
+    
+    
     #Draw Window
-    listwindow.mainloop()
+    logwindow.mainloop()
     return
 
 def commitformat(root):
