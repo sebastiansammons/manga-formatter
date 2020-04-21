3ee7765d4b3bf76bc8164b92f929e1b8adbdce42
Sebastian Sammons <ssammons92@gmail.com>
2020-04-21 11:29:51 -0700

    test branch changes

--- mangaconfig.py
+++ mangaconfig.py
@@ -8,13 +8,23 @@
 # MYSQLITEDB = "/Users/fridge/Development/Manga Format Project/fridgemediatest.db"
 
 #2018 Mac mini production
-ROOTPATH = "/Users/fridge/Manga/Manga/"
-QUEUESRC = "/Users/fridge/Manga/Formats/QUEUE/"
-MANUALDEST = "/Users/fridge/Manga/Formats/DESTINATION/"
-CHAPTERFORMATPATH = "/Chapter Pictures/New Chapters/"
-VOLUMEFORMATPATH = "/Chapter Pictures/Volume Chapters/"
-OPCHAPTERCOVERPATH = "/Users/fridge/Manga/Manga/One Piece/Cover Pictures/"
-MYSQLITEDB = "/Users/fridge/Manga/Formats/fridgemedia.db"
+# ROOTPATH = "/Users/fridge/Manga/Manga/"
+# QUEUESRC = "/Users/fridge/Manga/Formats/QUEUE/"
+# MANUALDEST = "/Users/fridge/Manga/Formats/DESTINATION/"
+# CHAPTERFORMATPATH = "/Chapter Pictures/New Chapters/"
+# VOLUMEFORMATPATH = "/Chapter Pictures/Volume Chapters/"
+# OPCHAPTERCOVERPATH = "/Users/fridge/Manga/Manga/One Piece/Cover Pictures/"
+# MYSQLITEDB = "/Users/fridge/Manga/Formats/fridgemedia.db"
+
+#2020 Gaming PC version testing
+ROOTPATH = "C:\Users\BassX\Code\Projects\Manga Formatter\Manga"
+QUEUESRC = "C:\Users\BassX\Code\Projects\Manga Formatter\Test\QUEUE"
+MANUALDEST = "C:\Users\BassX\Code\Projects\Manga Formatter\Test\DESTINATION"
+CHAPTERFORMATPATH = "\Chapter Pictures\New Chapters\"
+VOLUMEFORMATPATH = "\Chapter Pictures\Volume Chapters\"
+OPCHAPTERCOVERPATH = "C:\Users\BassX\Code\Projects\Manga Formatter\Manga\One Piece\Cover Pictures"
+MYSQLITEDB = "C:\Users\BassX\Code\Projects\Manga Formatter\Testfridgemedia.db"
+
 
 
 
