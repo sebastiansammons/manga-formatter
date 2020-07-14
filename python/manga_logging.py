b62d5435f27977f6d73b34bdcbf8d2954a220506
Sebastian Sammons <ssammons92@gmail.com>
2020-07-13 22:06:47 -0700

    Removed commented code

--- python/manga_logging.py
+++ python/manga_logging.py
@@ -9,17 +9,10 @@ ERROR_MSG = ""
 
 def config():
     try:
-        # logging.basicConfig(level=logging.DEBUG)
-        #logging.basicConfig(level=logging.DEBUG, filename=manga_config.LOG_PATH + "logs.log")
-        #logging.basicConfig(level=logging.DEBUG, format = '%(asctime)s:%(levelname)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S', )
         logging.basicConfig(level=logging.DEBUG, filename=manga_config.LOG_PATH + "testing_logs.log", format = '%(asctime)s|%(levelname)-7s|%(message)s', datefmt='%Y/%m/%d %H:%M', )
-        # logging.basicConfig(level=logging.DEBUG, filename=manga_config.LOG_PATH + "testing_logs.log", format = '%(asctime)s|%(levelname)-7s| %(message)s', datefmt='%Y/%m/%d %H:%M', )
-        #logging.basicConfig(level=logging.DEBUG, level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
-        #logging.basicConfig(level=logging.DEBUG, filename="test.log", format='%(asctime)s:%(levelname)s:%(message)s')
-        #logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(module)s:%(funcName)s:%(levelname)s:%(lineno)s:%(message)s)' )
-        #logging.basicConfig(level=logging.DEBUG, filename="test.log", filemode='a', format='%(asctime)s:%(module)s:%(funcName)s:%(levelname)s:%(lineno)s:%(message)s)' )
         logging.getLogger
     except FileNotFoundError:
+        logging.basicConfig(level=logging.DEBUG, datefmt='%Y/%m/%d %H:%M', )
         logging.basicConfig(level=logging.DEBUG)
         logging.getLogger
 
