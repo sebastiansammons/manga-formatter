52c3d780f81e9cfab7c3d2dcf075b844cbf17454
sebastiansammons <ssammons92@gmail.com>
2021-06-09 20:04:08 -0700

    fix config

--- src/manga/manga_config.py
+++ src/manga/manga_config.py
@@ -1,20 +1,19 @@
 #manga_config.py
 #FolderPaths:
-# MANGA_PATH = "/manga/"
-# SOURCE_PATH = "/manga/other/source/"
-# DESTINATION_PATH = "/manga/other/destination/"
-# LOGS_PATH = "/appdata/logs/"
-# LOGS_PATH = "./manga-formatter/tests/data/logs/"
-# DB_FILE_PATH = "/appdata/sqlite/fridge_manga.db"
+MANGA_PATH = "/manga/"
+SOURCE_PATH = "/manga/other/source/"
+DESTINATION_PATH = "/manga/other/destination/"
+LOGS_PATH = "/appdata/logs/"
+DB_FILE_PATH = "/appdata/sqlite/fridge_manga.db"
 
 #SubFolderPaths:
 NEW_CHAPTERS_SUBPATH = "/Chapter Pages/Newly Released/"
 VOLUMES_SUBPATH = "/Chapter Pages/Volumes/"
 OP_COVER_SUBPATH = "/Cover Pages/"
 
-#TestingPaths:
-MANGA_PATH = "/container/manga/"
-SOURCE_PATH = "/container/source/"
-DESTINATION_PATH = "/container/destination/"
-LOGS_PATH = "/container/logs/"
-DB_FILE_PATH = "/container/db/test_manga.db"
+# #TestingPaths:
+# MANGA_PATH = "/container/manga/"
+# SOURCE_PATH = "/container/source/"
+# DESTINATION_PATH = "/container/destination/"
+# LOGS_PATH = "/container/logs/"
+# DB_FILE_PATH = "/container/db/test_manga.db"
