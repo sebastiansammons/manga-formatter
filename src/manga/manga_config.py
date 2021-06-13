49b8af43791d42047db14200af7cb40ed2088325
sebastiansammons <ssammons92@gmail.com>
2021-06-13 13:01:12 -0700

    add environment variable option for config paths

--- src/manga/manga_config.py
+++ src/manga/manga_config.py
@@ -1,26 +1,13 @@
 #manga_config.py
+import os
 #FolderPaths:
-# MANGA_PATH = "/manga/"
-# SOURCE_PATH = "/manga/other/source/"
-# DESTINATION_PATH = "/manga/other/destination/"
-# LOGS_PATH = "/appdata/logs/"
-# DB_FILE_PATH = "/appdata/sqlite/fridge_manga.db"
+MANGA_PATH = os.getenv("MANGA_PATH", "/manga/")
+SOURCE_PATH = os.getenv("SOURCE_PATH", "/manga/other/source/")
+DESTINATION_PATH = os.getenv("DESTINATION_PATH", "/manga/other/destination/")
+LOGS_PATH = os.getenv("LOGS_PATH", "/appdata/logs/")
+DB_FILE_PATH = os.getenv("DB_FILE_PATH", "/appdata/sqlite/fridge_manga.db")
 
 #SubFolderPaths:
-NEW_CHAPTERS_SUBPATH = "/Chapter Pages/Newly Released/"
-VOLUMES_SUBPATH = "/Chapter Pages/Volumes/"
-OP_COVER_SUBPATH = "/Cover Pages/"
-
-# #TestingPaths (Container):
-# MANGA_PATH = "/container/manga/"
-# SOURCE_PATH = "/container/source/"
-# DESTINATION_PATH = "/container/destination/"
-# LOGS_PATH = "/container/logs/"
-# DB_FILE_PATH = "/container/db/test_manga.db"
-
-#TestingPaths (Command Line)
-MANGA_PATH = "./tests/data/container/manga/"
-SOURCE_PATH = "./tests/data/container/source/"
-DESTINATION_PATH = "./tests/data/container/destination/"
-LOGS_PATH = "./tests/data/container/logs/"
-DB_FILE_PATH = "./tests/data/container/db/test_manga.db"
+NEW_CHAPTERS_SUBPATH = os.getenv("NEW_CHAPTERS_SUBPATH", "/Chapter Pages/Newly Released/")
+VOLUMES_SUBPATH = os.getenv("VOLUMES_SUBPATH", "/Chapter Pages/Volumes/")
+OP_COVER_SUBPATH = os.getenv("OP_COVER_SUBPATH", "/Cover Pages/")   
