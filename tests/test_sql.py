d93245c19b1fcbfb1aeca4b5d67a73e9a8d31532
sebastiansammons <ssammons92@gmai.com>
2022-03-04 23:51:58 -0800

    unit tests fix

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -2,11 +2,11 @@
 import unittest
 # Before
 # import src.manga.manga_config as manga_config
-# import src.manga.manga_status as manga_status
+# import src.manga.manga_sql as manga_sql
 import sys
 sys.path.append("..")
 # from src.manga.manga_config import manga_config
-# from src.manga.manga_status import manga_status
+# from src.manga.manga_sql import manga_sql
 from src.manga import manga_config
 from src.manga import manga_sql
 
@@ -18,20 +18,20 @@ class TestStatus(unittest.TestCase):
 
     def test_active_manga(self):
         expected_active_manga = ["Attack on Titan", "Blue Box", "My Hero Academia", "One Piece"]
-        active_manga = manga_status.get_active_manga()
+        active_manga = manga_sql.get_active_manga()
         self.assertEqual(active_manga, expected_active_manga)
 
     def test_chapter_status(self):
         expected_active_manga = ["Attack on Titan", "Blue Box", "My Hero Academia", "One Piece"]
         expected_manga_chapter = [139, 6, 313, 1013]
-        active_manga, manga_chapter = manga_status.get_chapter_status()
+        active_manga, manga_chapter = manga_sql.get_chapter_status()
         self.assertEqual(active_manga, expected_active_manga)
         self.assertEqual(manga_chapter, expected_manga_chapter)
 
     def test_volume_status(self):
         expected_active_manga = ["Attack on Titan", "Blue Box", "My Hero Academia", "One Piece"]
         expected_manga_volume = [33, 0, 30, 98]
-        active_manga, manga_volume = manga_status.get_volume_status()
+        active_manga, manga_volume = manga_sql.get_volume_status()
         self.assertEqual(active_manga, expected_active_manga)
         self.assertEqual(manga_volume, expected_manga_volume)
 
