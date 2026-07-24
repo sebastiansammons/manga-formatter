a8582c5937d23746d17b4d07cf942191e48f59df
Fridge <fridge@fridge.fridgemedia>
2026-07-24 08:44:42 -0700

    src/epub/epub_config.py

--- src/manga/manga_config.py
+++ src/manga/manga_config.py
@@ -10,7 +10,7 @@ ERROR_PATH = os.getenv("ERROR_PATH", "/appdata/error_msg/")
 DB_FILE_PATH = os.getenv("DB_FILE_PATH", "/appdata/sqlite/fridge_manga.db")
 
 # SubDirectory Paths:
-NEW_CHAPTERS_SUBPATH = os.getenv("NEW_CHAPTERS_SUBPATH", "/Chapter Pages/Newly Released/")
-VOLUMES_SUBPATH = os.getenv("VOLUMES_SUBPATH", "/Chapter Pages/Volumes/")
-EPUB_VOLUMES_SUBPATH = os.getenv("EPUB_VOLUMES_SUBPATH", "/Volumes/")
-OP_COVER_SUBPATH = os.getenv("OP_COVER_SUBPATH", "/Cover Pages/")
+NEW_CHAPTERS_SUBPATH = os.getenv("NEW_CHAPTERS_SUBPATH", "Chapter Pages/Newly Released")
+VOLUMES_SUBPATH = os.getenv("VOLUMES_SUBPATH", "Chapter Pages/Volumes")
+EPUB_VOLUMES_SUBPATH = os.getenv("EPUB_VOLUMES_SUBPATH", "Volumes")
+OP_COVER_SUBPATH = os.getenv("OP_COVER_SUBPATH", "Cover Pages")
